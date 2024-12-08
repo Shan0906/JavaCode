6e9a56a4df4174aa9a1a63b479798218a834872c
Shan0906 <mohod.shantanu09@gmail.com>
2024-12-08 06:13:29 -0800

    Third

---
+++ Shan_1.java
@@ -0,0 +1,14 @@
+package W_Access1;
+
+import W_Access2.Shan_2;
+
+public class Shan_1
+{
+	public static void main(String[] args)         //Access by creating object with diffrent package
+	{
+		Shan_2 s1=new Shan_2();
+		s1.Sub();
+		
+		
+	}
+}
